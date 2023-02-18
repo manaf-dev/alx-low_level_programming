939bdefb01bd23abf564100f7961384c32f8ba53
manaf <manaf18.m@gmail.com>
2023-02-18 04:11:08 -0800

    lower and upper alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - outputs lower and upper alphabets
+ * Return: 0 is returned
+ *
+ * Destription: print alphabets
+ */
+int main(void)
+{
+	int letter_1 = 'a';
+	int letter_2 = 'A';
+
+	do {
+		putchar(letter_1);
+		letter_1++;
+	} while (letter_1 <= 'z');
+
+	do {
+		putchar(letter_2);
+		letter_2++;
+	} while (letter_2 <= 'Z');
+	
+	putchar('\n');
+
+	return (0);
+}
