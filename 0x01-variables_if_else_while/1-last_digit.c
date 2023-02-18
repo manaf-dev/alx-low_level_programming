973bbdf5c2e277b082ea2abf05eb9fe920118cf0
manaf <manaf18.m@gmail.com>
2023-02-18 03:18:34 -0800

    greater than 6 or less and 6 and not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,14 +19,14 @@ int main(void)
 	/* your code goes here */
 	l = n % 10;
 	if (l > 5)
-	{        
+	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-        else if (l < 6 && l != 0)
+	else if (l < 6 && l != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-        }
-        else
+	}
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
