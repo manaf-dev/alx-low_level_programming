b98fd71f74ff212a8a4f5fae59b82d7a5921b206
manaf <manaf18.m@gmail.com>
2023-02-18 03:07:57 -0800

    greater than 6 or less and 6 and not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,17 +16,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here */
-	if (n > 5)
+	int l;
+	l = n % 10;
+	if (l > 5)
         {
-                printf("Last digit of %d is %d and is greater than 5\n", n,n%10);
+       		printf("Last digit of %d is %d and is greater than 5\n", n, l);
         }
-        else if (n < 6 && n != 0)
+        else if (l < 6 && l != 0)
         {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n,n%10);
+        	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
         }
         else
 	{
-                printf("Last digit of %d is %d and is 0\n", n,n%10);
+        	printf("Last digit of %d is %d and is 0\n", n, l);
         }
 	return (0);
 }
