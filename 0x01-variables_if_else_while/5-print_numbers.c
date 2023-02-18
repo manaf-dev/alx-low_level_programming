30446cd0850869c1eeba8dfa5491c5fbc3bb91ae
manaf <manaf18.m@gmail.com>
2023-02-18 04:46:16 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - outputs base 10 single numbers
+ * Return: 0 is returned
+ *
+ * Destription: print single numbers from 0
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
